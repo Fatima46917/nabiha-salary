778b569ee1b9072a5fbd4c05ef5da6840da04946
fatima sharaf eddine <fatumash44@gmail.com>
2025-02-21 21:59:45 +0200

    salary + month name + monthlyfinances ( savings ,rent ,electricity)

---
+++ script.py
@@ -0,0 +1,7 @@
+salary = input("please enter the salary of the month :")
+
+name = input ("please enter the name of the month :")
+
+monthlyfinances = ["savings ,Rnet ,electricity"]
+monthlyfinances.append(input("Enter the percentage and amount combined of savings + rent + electricity : "))
+print (monthlyfinances)
